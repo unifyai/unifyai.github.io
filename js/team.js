6776c3c9e2eaa292327a39f47ee5b5cae1bec328
Ved <vedpat3@gmail.com>
2022-08-19 11:33:26 +0530

    Added svgs and alignment of team cards

---
+++ js/team.js
@@ -0,0 +1,94 @@
+const team = [
+  {
+    image: "../img/logos/ivy_logo_only.png",
+    name: "Name Name",
+    bio: "CS Undergraduate at UCL, [something personal about you>].",
+    github: "namename",
+    discord: "xxxx",
+  },
+  {
+    image: "../img/logos/ivy_logo_only.png",
+    name: "Name Name",
+    bio: "CS Undergraduate at UCL, [something personal about you>].",
+    github: "namename",
+    discord: "xxxx",
+  },
+  {
+    image: "../img/logos/ivy_logo_only.png",
+    name: "Name Name",
+    bio: "CS Undergraduate at UCL, [something personal about you>].",
+    github: "namename",
+    discord: "xxxx",
+  },
+  {
+    image: "../img/logos/ivy_logo_only.png",
+    name: "Name Name",
+    bio: "CS Undergraduate at UCL, [something personal about you>].",
+    github: "namename",
+    discord: "xxxx",
+  },
+  {
+    image: "../img/logos/ivy_logo_only.png",
+    name: "Name Name",
+    bio: "CS Undergraduate at UCL, [something personal about you>].",
+    github: "namename",
+    discord: "xxxx",
+  },
+  {
+    image: "../img/logos/ivy_logo_only.png",
+    name: "Name Name",
+    bio: "CS Undergraduate at UCL, [something personal about you>].",
+    github: "namename",
+    discord: "xxxx",
+  },
+  {
+    image: "../img/logos/ivy_logo_only.png",
+    name: "Name Name",
+    bio: "CS Undergraduate at UCL, [something personal about you>].",
+    github: "namename",
+    discord: "xxxx",
+  },
+  {
+    image: "../img/logos/ivy_logo_only.png",
+    name: "Name Name",
+    bio: "CS Undergraduate at UCL, [something personal about you>].",
+    github: "namename",
+    discord: "xxxx",
+  },
+  {
+    image: "../img/logos/ivy_logo_only.png",
+    name: "Name Name",
+    bio: "CS Undergraduate at UCL, [something personal about you>].",
+    github: "namename",
+    discord: "xxxx",
+  },
+  {
+    image: "../img/logos/ivy_logo_only.png",
+    name: "Name Name",
+    bio: "CS Undergraduate at UCL, [something personal about you>].",
+    github: "namename",
+    discord: "xxxx",
+  },
+];
+
+const team_container = document.querySelector(".team-container");
+
+for (let member of team) {
+  const card = document.createElement("div");
+  card.classList.add("cards");
+  card.classList.add("col-md-6");
+  card.innerHTML = `
+    <div class="card">
+      <div class="image-container">
+          <img src="${member["image"]}" alt="image" class="image" />
+      </div>
+      <div class="card-content">
+          <div class="name">${member["name"]}</div>
+          <div class="bio">${member["bio"]}</div>
+          <div class="github"><a href="https://github.com/${member["github"]}" target="_blank"><img src = "../font-awesome/svgs/github-brands.svg" width="25" height="25" class="profile-icon"/>github.com/${member["github"]}</a></div>
+          <div class="discord"><a href="https://discord.com/users/${member["discord"]}" target="_blank"><img src = "../font-awesome/svgs/discord.svg" width="25" height="25" class="profile-icon"/>discord.com/users/${member["discord"]}</a></div>
+      </div>
+    </div>
+    `;
+  team_container.appendChild(card);
+}
